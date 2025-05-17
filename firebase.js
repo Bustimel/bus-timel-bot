fa0319c4e7228a46791fd023bc77478db348fec9
Bustimel <bustimelll@gmail.com>
2025-05-17 18:35:57 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
 
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+// Чтение ключа из переменной среды
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
